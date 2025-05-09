0ad1d1a96339b1372d77e3ebc5f2366b308ec707
Nyabsi <nyabsi@sovellus.cc>
2025-05-09 10:19:14 +0000

    add intel platform requirements

--- Casks/a/amethyst-player.rb
+++ Casks/a/amethyst-player.rb
@@ -5,6 +5,13 @@ cask "amethyst-player" do
   sha256 arm:   "3c743301a9a5547e787d5e7ddb3dadc17d9c747e4f85a0b18091b6fc6c09010e",
          intel: "c8e02048c1de7ee36d625192b474bc13bd01dbed3cae01f2f12e9e27eac42914"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :mojave"
+  end
+
   url "https://github.com/Geoxor/Amethyst/releases/download/v#{version}/Amethyst-#{version}#{arch}.dmg"
   name "Amethyst"
   desc "Cross-platform audio player with a node-based audio routing system"
@@ -14,8 +21,6 @@ cask "amethyst-player" do
     url :url
     strategy :github_releases
   end
-
-  depends_on macos: ">= :big_sur"
 
   app "Amethyst.app"
 
