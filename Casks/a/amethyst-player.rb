1707c38c29ebc4ee1e89e63d4b98111a49ebc5ad
Nyabsi <nyabsi@sovellus.cc>
2025-05-09 10:21:31 +0000

    set hash in platform specific functions

--- Casks/a/amethyst-player.rb
+++ Casks/a/amethyst-player.rb
@@ -2,13 +2,13 @@ cask "amethyst-player" do
   arch arm: "-arm64", intel: ""
 
   version "2.1.2"
-  sha256 arm:   "3c743301a9a5547e787d5e7ddb3dadc17d9c747e4f85a0b18091b6fc6c09010e",
-         intel: "c8e02048c1de7ee36d625192b474bc13bd01dbed3cae01f2f12e9e27eac42914"
 
   on_arm do
+    sha256 "3c743301a9a5547e787d5e7ddb3dadc17d9c747e4f85a0b18091b6fc6c09010e"
     depends_on macos: ">= :big_sur"
   end
   on_intel do
+    sha256 "c8e02048c1de7ee36d625192b474bc13bd01dbed3cae01f2f12e9e27eac42914"
     depends_on macos: ">= :mojave"
   end
 
