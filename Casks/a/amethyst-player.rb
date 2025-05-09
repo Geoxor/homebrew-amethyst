ebcf2d7ac2341466f8c46a957075799b2b0f0599
Nyabsi <nyabsi@sovellus.cc>
2025-05-09 10:24:34 +0000

    make lint happy

--- Casks/a/amethyst-player.rb
+++ Casks/a/amethyst-player.rb
@@ -5,10 +5,12 @@ cask "amethyst-player" do
 
   on_arm do
     sha256 "3c743301a9a5547e787d5e7ddb3dadc17d9c747e4f85a0b18091b6fc6c09010e"
+
     depends_on macos: ">= :big_sur"
   end
   on_intel do
     sha256 "c8e02048c1de7ee36d625192b474bc13bd01dbed3cae01f2f12e9e27eac42914"
+
     depends_on macos: ">= :mojave"
   end
 
